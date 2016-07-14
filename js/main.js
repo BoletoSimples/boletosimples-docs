99f7e1fa068cda5f055ae4f0c8c73c5b72a76df3
Mikael Carrara <mikaelmorattocarrara@gmail.com>
2016-07-14 19:58:30 -0300

    Styled sidr

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
 Main = {
   init: function(){
 
-    // Mobile menu
 
-$("#user_email").focus();
+    // mobile menu
 
-    /* Mobile Nav */
+    $("#user_email").focus();
+
+
+    /* mobile nav */
 
     $("#menu-trigger").click(function() {
       $("#main-header nav ul").toggle();
       $(".header-form").hide();
     });
+
+
+    /* mobile sidebar */
+
+    $('#simple-menu').sidr();
 
   }
 }
