56b660140a00e4e830e91b83dcd8c128b759cc29
Mikael Carrara <mikaelmorattocarrara@gmail.com>
2016-07-14 17:51:16 -0300

    Removed login script

--- js/main.js
+++ js/main.js
@@ -5,26 +5,6 @@ Main = {
 
 $("#user_email").focus();
 
-    /* Login Dropdown */
-
-    $(".login-trigger").mouseover(function() {
-      $("#main-header nav ul").hide();
-      $(".header-form").show();
-      if(!$("#login_email").val()) { $("#login_email").focus(); }
-      else { $("#login_password").focus(); }
-    });
-
-    $(".signup-trigger").mouseover(function() {
-      $("#main-header nav ul").hide();
-      $(".header-form").show();
-      if(!$("#signup_email").val()) { $("#signup_email").focus(); }
-    });
-
-    $(".back-arrow").click(function() {
-      $(".header-form").hide();
-    });
-
-
     /* Mobile Nav */
 
     $("#menu-trigger").click(function() {
